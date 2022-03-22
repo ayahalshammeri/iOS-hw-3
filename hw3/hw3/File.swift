818acf74606997727cec6e78f58718ea687be28c
AyahAlshammeri <66526830+jumana-alshameri@users.noreply.github.com>
2022-03-22 17:55:48 +0300

    done

---
+++ hw3/hw3/File.swift
@@ -0,0 +1,23 @@
+//
+//  File.swift
+//  hw3
+//
+//  Created by jumana alshameri on 21/03/2022.
+//
+
+import Foundation
+import SwiftUI
+struct Expenses: Identifiable{
+            var id = UUID()
+            var store : String
+            var paid : Double
+            var profileImage : String
+            var purchases : String
+        }
+
+    var expensesArray=[
+        Expenses(store: "zara", paid: 21.350, profileImage:"zara", purchases: "قميص ابيض"),
+        Expenses(store: "adidas", paid:30.000, profileImage: "adidas", purchases:" حذاء ابيض"),
+        Expenses(store: "gait", paid: 570.950, profileImage: "gait", purchases: "apple macBook pro"),
+        Expenses(store: "saray", paid: 43.000, profileImage: "saray", purchases: "عطر")
+    ]
---
+++ hw3/hw3/hw3App.swift
@@ -0,0 +1,17 @@
+//
+//  hw3App.swift
+//  hw3
+//
+//  Created by jumana alshameri on 20/03/2022.
+//
+
+import SwiftUI
+
+@main
+struct hw3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
